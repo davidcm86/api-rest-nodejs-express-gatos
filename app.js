a78db9d3f1bfaaf71f5b0d7dd07f39e01a265821
davidcm86 <dcarretero86@hotmail.com>
2015-03-21 21:28:57 +0100

    Api Gatos en su version mas sencilla terminada

--- app.js
+++ app.js
@@ -1,20 +1,44 @@
-var express = require("express"),
-	app = express(),
-	bodyParser = require("body-parser"),
+var express = require("express"), // nos permite gesticionar todo lo relacionado con hhtp, se compenetra con nodejs
+	app = express(), // en app tendremos express para utilizarlo
+	bodyParser = require("body-parser"), // permite gestionar las peticiones que se reciben
 	methodOverride = require("method-override"),
-	mongoose = require('mongoose');
+	mongoose = require('mongoose'); // permite gestionar la bbdd en mongo
 
-// Middlewares
+// Conectamos con la BBDD
+mongoose.connect('mongodb://localhost/gatos', function(err, res) {
+	if(err) throw err;
+	console.log('Conexion a al BBDD exitosa');
+});
+
+// Los Middlewares
 app.use(bodyParser.urlencoded({ extended: false })); // for parsing application/x-www-form-urlencoded
 app.use(bodyParser.json()); // for parsing application/json
-app.use(methodOverride());
+//app.use(methodOverride()); // permite utilizar los verbos cuando el cliente no lo soporte
 
-// Route index
+// Importamos modelos
+var models = require('./models/gato')(app, mongoose);
+
+// Importamos controladores
+var GatosCtrl = require('./controllers/gatos');
+
+// Ruta de Inicio
 var router = express.Router();
-	router.get('/', function(req, res) {
-	res.send("Hello world!!!!");
+router.get('/', function(req, res) {
+	res.send("Bienvenidos a la Api Rest de Gatos");
 });
 app.use(router);
+
+// Rutas de la API
+var gatos = express.Router();
+gatos.route('/gatos')
+	.get(GatosCtrl.dameGatos)
+	.post(GatosCtrl.metoGato);
+
+gatos.route('/gatos/:id')
+	.get(GatosCtrl.dameGato)
+	.put(GatosCtrl.modificarGato)
+	.delete(GatosCtrl.deleteGato);	
+app.use('/', gatos);
 
 // Start server
 app.listen(3000, function() {
